7cd6a69c627473e2b910ebdad562a58d9fbc5955
byronpantoja <byronpantoja@gmail.com>
2023-10-01 07:31:42 +0800

    setting up project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -15,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>{children}</body>
     </html>
   )
