905525b2b221d2bda17bad4b2e7d5a16bf8637d1
byronpantoja <byronpantoja@gmail.com>
2023-10-03 05:56:19 +0800

    adding auth routes va clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
     <div>
-      <h1 className='h1-bold'>Next 13</h1>
+      Home
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
