ef6b1a5ca12e7ba26f94e507e274aafc94a54bc8
byronpantoja <byronpantoja@gmail.com>
2023-10-03 15:55:02 +0800

    adding favicon and custom fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import { ClerkProvider } from '@clerk/nextjs'
- 
-export const metadata = {
+import { Inter, Space_Grotesk as SpaceGrotesk } from 'next/font/google'
+
+import type { Metadata } from 'next'
+
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+})
+
+const spaceGrotesk = SpaceGrotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spacerotesk',
+})
+
+export const metadata: Metadata = {
   title: 'Stack Overflow Clone in Next.js 13',
+  description: 'Stack Overflow Clone in Next.js 13',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  },
 }
- 
+
 export default function RootLayout({
   children,
 }: {
@@ -12,8 +33,10 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body>{children}</body>
+      <html lang='en'>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
