05cf27de3371c8a653398e76581d0256fe64fa6e
byronpantoja <byronpantoja@gmail.com>
2024-01-18 18:03:35 +0800

    adding tags

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -2,7 +2,8 @@
 
 import User from '@/database/user.model'
 import { connectToDatabase } from '../mongoose'
-import { GetTopInteractedTagsParams } from './shared.types'
+import { GetAllTagsParams, GetTopInteractedTagsParams } from './shared.types'
+import Tag from '@/database/tag.model'
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -12,16 +13,28 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
 
     const user = await User.findById(userId)
 
-    if (!user) {
-      throw new Error('User not found')
-    }
+    if (!user) throw new Error('User not found')
 
-    // TODO: Find interactions for the user and group by tags.Interaction...
+    // Find interactions for the user and group by tags...
+    // Interaction...
 
     return [
-      { _id: '1', name: 'tag1' },
+      { _id: '1', name: 'tag' },
       { _id: '2', name: 'tag2' },
     ]
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase()
+
+    const tags = await Tag.find({})
+
+    return { tags }
   } catch (error) {
     console.log(error)
     throw error
