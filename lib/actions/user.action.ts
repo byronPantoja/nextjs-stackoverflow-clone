6e8933aed537d3174f14c6bc6468e46a9553a702
byronpantoja <byronpantoja@gmail.com>
2024-01-18 17:28:55 +0800

    adding community page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,66 +1,71 @@
-"use server"
+'use server'
 
-import User from "@/database/user.model";
-import { connectToDatabase } from "../mongoose"
-import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
-import { revalidatePath } from "next/cache";
-import Question from "@/database/question.model";
+import User from '@/database/user.model'
+import { connectToDatabase } from '../mongoose'
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  GetAllUsersParams,
+  UpdateUserParams,
+} from './shared.types'
+import { revalidatePath } from 'next/cache'
+import Question from '@/database/question.model'
 
 export async function getUserById(params: any) {
   try {
-    connectToDatabase();
+    connectToDatabase()
 
-    const { userId } = params;
+    const { userId } = params
 
-    const user = await User.findOne({ clerkId: userId });
+    const user = await User.findOne({ clerkId: userId })
 
-    return user;
+    return user
   } catch (error) {
-    console.log(error);
-    throw error;
+    console.log(error)
+    throw error
   }
 }
 
 export async function createUser(userData: CreateUserParams) {
   try {
-    connectToDatabase();
+    connectToDatabase()
 
-    const newUser = await User.create(userData);
+    const newUser = await User.create(userData)
 
-    return newUser;
+    return newUser
   } catch (error) {
-    console.log(error);
-    throw error;
+    console.log(error)
+    throw error
   }
 }
 
 export async function updateUser(params: UpdateUserParams) {
   try {
-    connectToDatabase();
+    connectToDatabase()
 
-    const { clerkId, updateData, path } = params;
+    const { clerkId, updateData, path } = params
 
     await User.findOneAndUpdate({ clerkId }, updateData, {
       new: true,
-    });
+    })
 
-    revalidatePath(path);
+    revalidatePath(path)
   } catch (error) {
-    console.log(error);
-    throw error;
+    console.log(error)
+    throw error
   }
 }
 
 export async function deleteUser(params: DeleteUserParams) {
   try {
-    connectToDatabase();
+    connectToDatabase()
 
-    const { clerkId } = params;
+    const { clerkId } = params
 
-    const user = await User.findOneAndDelete({ clerkId });
+    const user = await User.findOneAndDelete({ clerkId })
 
-    if(!user) {
-      throw new Error('User not found');
+    if (!user) {
+      throw new Error('User not found')
     }
 
     // Delete user from database
@@ -70,15 +75,39 @@ export async function deleteUser(params: DeleteUserParams) {
     // const userQuestionIds = await Question.find({ author: user._id}).distinct('_id');
 
     // delete user questions
-    await Question.deleteMany({ author: user._id });
+    await Question.deleteMany({ author: user._id })
 
     // TODO: delete user answers, comments, etc.
 
-    const deletedUser = await User.findByIdAndDelete(user._id);
+    const deletedUser = await User.findByIdAndDelete(user._id)
 
-    return deletedUser;
+    return deletedUser
   } catch (error) {
-    console.log(error);
-    throw error;
+    console.log(error)
+    throw error
   }
 }
+
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase()
+
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params
+
+    const users = await User.find({}).sort({ createdAt: -1 })
+    return { users }
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+// export async function getAllUsers(params: GetAllUsersParams) {
+//   try {
+//     connectToDatabase();
+
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }
